5ede83f11a15b5b9c4d93922017cbbc7b2547a89
Diego Lorca Chacon <diego.lorca.chacon@gmail.com>
2018-12-12 02:16:02 -0300

    create project week19 activity37

---
+++ db/migrate/20180904231712_create_employees.rb
@@ -0,0 +1,12 @@
+class CreateEmployees < ActiveRecord::Migration[5.1]
+  def change
+    create_table :employees do |t|
+      t.string :name
+      t.string :last_name
+      t.string :occupation
+      t.integer :age
+
+      t.timestamps
+    end
+  end
+end
